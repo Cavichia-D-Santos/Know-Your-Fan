935350e35b39e5a8be34b6438fba4e07a8093d61
Cavichia-D-Santos <thiagogamer10000gmail.com>
2025-05-04 16:20:38 -0300

    rotas para imagens ts

---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import home from "@/assets/icons/home.png";
+import quest from "@/assets/icons/quest.png";
+import trophy from "@/assets/icons/trophy.png";
+import user from "@/assets/icons/user.png";
+
+export const icons = {
+    home,
+    quest,
+    trophy,
+    user,
+};
